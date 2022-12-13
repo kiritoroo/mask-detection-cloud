d9c18bfef09509c7b9f99c6dfe93a0a95ecd4fb8
Le Trung <trungle30902@gmail.com>
2022-12-13 16:13:11 +0700

    [ 221213 ] ☁️ - fix members

--- service/constant.py
+++ service/constant.py
@@ -1,19 +1,19 @@
 members = [
     {
         "id": "20110587", 
-        "name": "Le Kien Trung", 
+        "name": "Lê Kiên Trung", 
         "github": "https://github.com/kiritoroo", 
         "avatar": "/static/3d-boy-avatar.webp"
     },
     {
         "id": "20110135", 
-        "name": "Lê Thi Thanh Tuyet", 
+        "name": "Lê Thị Thanh Tuyết", 
         "github": "https://github.com/Snow-3", 
         "avatar": "/static/3d-girl-avatar.webp"
     },
     {
         "id": "20119339", 
-        "name": "Ho Xuan Hieu", 
+        "name": "Hồ Xuân Hiếu", 
         "github": "https://github.com/XuanHieuHo", 
         "avatar": "/static/3d-boy-avatar.webp"
     }
