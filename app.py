a150539c9ca76bc43866791e7d5d151964dd632a
Le Trung <trungle30902@gmail.com>
2022-12-13 14:36:58 +0700

    [ 221213 ] ☁️ - convert jinja template

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import uvicorn
 from fastapi import File, UploadFile, Request, FastAPI
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from service.constant import *
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -11,12 +13,12 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 def main(request: Request):
     return templates.TemplateResponse(
         "index.html",
-        {"request": request})
+        {"request": request, "members": members})
 
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host='127.0.0.1',
-        port=8080,
+        host='0.0.0.0',
+        port=int(os.environ.get('APP_POST')) or 8080,
         log_level="info",
         reload=True)
---
+++ service/constant.py
@@ -0,0 +1,20 @@
+members = [
+    {
+        "id": "20110587", 
+        "name": "Le Kien Trung", 
+        "github": "https://github.com/kiritoroo", 
+        "avatar": "/static/3d-boy-avatar.webp"
+    },
+    {
+        "id": "20110135", 
+        "name": "Nguyen Thi Thanh Tuyet", 
+        "github": "https://github.com/Snow-3", 
+        "avatar": "/static/3d-girl-avatar.webp"
+    },
+    {
+        "id": "20119339", 
+        "name": "Ho Xuan Hieu", 
+        "github": "https://github.com/XuanHieuHo", 
+        "avatar": "/static/3d-boy-avatar.webp"
+    }
+]
